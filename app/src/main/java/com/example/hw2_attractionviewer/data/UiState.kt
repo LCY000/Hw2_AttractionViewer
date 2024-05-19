1add4d4ac81b2fad7ba2d94a9187b8b947a235eb
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-05-19 17:24:56 +0800

    Uistate

---
+++ app/src/main/java/com/example/hw2_attractionviewer/data/UiState.kt
@@ -0,0 +1,5 @@
+package com.example.hw2_attractionviewer.data
+
+data class UiState(
+    val dataNumber: Int = 0
+)
---
+++ app/src/main/java/com/example/hw2_attractionviewer/ui/theme/ViewModle.kt
@@ -0,0 +1,23 @@
+package com.example.hw2_attractionviewer.ui.theme
+
+import androidx.lifecycle.ViewModel
+import com.example.hw2_attractionviewer.data.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class ViewModle: ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState : StateFlow<UiState> =_uiState.asStateFlow()
+
+    fun setDataNumber(inputNum: Int){
+        _uiState.update {currentState ->
+            currentState.copy(
+                dataNumber = inputNum
+            )
+        }
+    }
+
+}
