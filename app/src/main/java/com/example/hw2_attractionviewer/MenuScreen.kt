f012191cbcd425a3ae6d86434dd1f876e172dc26
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-05-20 02:55:44 +0800

    第一頁的介面幾乎完成，只剩跳轉到第二頁這步驟

--- app/src/main/java/com/example/hw2_attractionviewer/MenuScreen.kt
+++ app/src/main/java/com/example/hw2_attractionviewer/MenuScreen.kt
@@ -2,8 +2,11 @@ package com.example.hw2_attractionviewer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,12 +20,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.example.hw2_attractionviewer.data.Place
 import com.example.hw2_attractionviewer.data.places
@@ -74,23 +81,33 @@ fun PlaceCard(
 ){
     Card(
         modifier= modifier.clickable { onClick() },
-        shape = RoundedCornerShape(16.dp)
+        shape = RoundedCornerShape(20.dp)
     ){
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp)
+                .padding(8.dp)
         ){
             Image(
                 painter = painterResource(id =place.imageResourceId),
                 contentDescription = null,
+//                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(80.dp)
-                    .padding(10.dp)
+                    .height(70.dp)
+                    .padding(1.dp)
                     .clip(RoundedCornerShape(16.dp))
                 )
 
             //還沒有景點名稱
+            Text(
+                text = stringResource(place.name),
+                modifier = modifier
+                    .padding(vertical= 12.dp)
+                    .padding(start = 24.dp)
+                    .fillMaxSize(),
+                textAlign = TextAlign.Start,
+                fontSize = 18.sp
+            )
         }
     }
 
