07b7f637b20e0936f09b41f330510ebdbcde9f01
LCY000 <84628528+LCY000@users.noreply.github.com>
2024-05-22 14:09:16 +0800

    完成

--- app/src/main/java/com/example/hw2_attractionviewer/MenuScreen.kt
+++ app/src/main/java/com/example/hw2_attractionviewer/MenuScreen.kt
@@ -2,18 +2,15 @@ package com.example.hw2_attractionviewer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,18 +19,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import com.example.hw2_attractionviewer.data.Place
 import com.example.hw2_attractionviewer.data.places
@@ -42,9 +35,7 @@ import com.example.hw2_attractionviewer.ui.theme.ViewerViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuAppBar(
-    modifier: Modifier = Modifier
-) {
+fun MenuAppBar() {
     TopAppBar(
         title = { Text(stringResource(R.string.app_name)) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -70,7 +61,7 @@ fun MenuScreen(
                     modifier = modifier,
                     place = place,
                     onClick = {
-                        viewModel.setDataNumber(index);
+                        viewModel.setDataNumber(index)
                         navController.navigate("detailPage")
                         },
                 )
